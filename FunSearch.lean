e5248fcde14cc5460a035ffec54312b88d98507f
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-02-24 16:42:24 +0530

    github action CI

--- FunSearch.lean
+++ FunSearch.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `FunSearch` library.
 -- Import modules here that should be built as part of the library.
-import «FunSearch».Basic
+import «FunSearch».Frontend
+import «FunSearch».ChatClient
