07a80a6f8c288129bf534b46a0a91f64914e54aa
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-04-12 20:49:15 +0530

    toolchain update

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,7 @@ package «FunSearch» where
   -- add any additional package configuration options here
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git"@"v4.7.0"
 
 @[default_target]
 lean_lib «FunSearch» where
